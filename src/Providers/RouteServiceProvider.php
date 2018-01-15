115d7502bfb4dfa5ca42a0623dcb5ab7b0e1bc1d
Vojin Janevski <vojin.janevski@logeecom.com>
2018-01-15 23:04:15 +0100

    [feature/SI-654]: SI-654 Fix authorization for custom api endpoints

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -4,16 +4,18 @@ namespace Newsletter2Go\Providers;
 
 use Newsletter2Go\Controllers\ApiController;
 use Plenty\Plugin\RouteServiceProvider as BaseRouteServiceProvider;
-use Plenty\Plugin\Routing\Router;
+use Plenty\Plugin\Routing\ApiRouter;
 
 class RouteServiceProvider extends BaseRouteServiceProvider
 {
-    public function map(Router $router)
+    public function map(ApiRouter $apiRouter)
     {
-        $controllerClass = ApiController::class;
-        $router->get('newsletter2go/test', $controllerClass . '@test');
-        $router->get('newsletter2go/version', $controllerClass . '@version');
-        $router->get('newsletter2go/count', $controllerClass . '@customerCount');
-        $router->get('newsletter2go/customers', $controllerClass . '@customers');
+        $apiRouter->version(['v1'], ['middleware' => 'oauth'], function ($apiRouter) {
+            $controllerClass = ApiController::class;
+            $apiRouter->get('newsletter2go/test', $controllerClass . '@test');
+            $apiRouter->get('newsletter2go/version', $controllerClass . '@version');
+            $apiRouter->get('newsletter2go/count', $controllerClass . '@customerCount');
+            $apiRouter->get('newsletter2go/customers', $controllerClass . '@customers');
+        });
     }
 }

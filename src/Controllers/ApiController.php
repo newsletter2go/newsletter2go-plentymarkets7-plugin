cb7dcc47645e151c96756b8669f33cfe23b7aacf
Maiky <mareike.ziese@gmx.net>
2018-06-06 10:37:50 +0200

    Revert "Revert "added pagination to email-folder-recipients""
    This reverts commit afba6ee704eb559fd25269c510be5094b25d68a5.

--- src/Controllers/ApiController.php
+++ src/Controllers/ApiController.php
@@ -87,7 +87,7 @@ class ApiController extends Controller
         if (strpos($group, 'newsletter_') === 0) {
             $group = str_replace('newsletter_', '', $group);
 
-            return $this->dataHelper->getRecipients(intval($group), $subscribed, $hours, $emails);
+            return $this->dataHelper->getRecipients(intval($group), $subscribed, $hours, $emails, $page, $limit);
         } else {
             return $this->dataHelper->getContacts(intval($group), $subscribed, $hours, $emails, $page, $limit);
         }

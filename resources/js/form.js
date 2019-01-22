30f53d0ca1cbd4dccd062453c10ab76335751de7
MiMo <moritz@newsletter2go.com>
2019-01-22 15:14:29 +0100

    test

--- resources/js/form.js
+++ resources/js/form.js
@@ -1,79 +1,160 @@
-<script id="n2g_script">
-var config = {
-    "form": {
-        "class": "",
-        "style": ""
-    },
-    "container": {
-        "type": "table",
-        "class": "",
-        "style": "width: 100%;"
-    },
-    "row": {
-        "type": "tr",
-        "class": "",
-        "style": ""
-    },
-    "columnLeft": {
-        "type": "td",
-        "class": "",
-        "style": "width: 40%; padding: 10px 5px;"
-    },
-    "columnRight": {
-        "type": "td",
-        "class": "",
-        "style": ""
-    },
-    "checkbox": {
-        "type": "input",
-        "class": "",
-        "style": ""
-    },
-    "separator": {
-        "type": "br",
-        "class": "",
-        "style": ""
-    },
-    "input": {
-        "class": "",
-        "style": "padding: 5px 10px; border-radius: 2px; border: 1px solid #d8dee4;"
-    },
-    "dropdown": {
-        "type": "select",
-        "class": "",
-        "style": "padding: 3px 5px; border-radius: 2px; border: 1px solid #d8dee4;"
-    },
-    "button": {
-        "type": "button",
-        "class": "",
-        "id": "",
-        "style": "background-color: #000000; border: none; border-radius: 4px; padding: 10px 20px; color: #ffffff; margin-top: 20px; cursor: pointer;"
-    },
-    "label": {
-        "type": "label",
-        "class": "",
-        "style": "padding-left: 5px"
-    },
-    "loader": {
-        "type": "img",
-        "src": "//www.newsletter2go.com/images/loader.svg",
-        "class": "",
-        "style": "margin: auto; display:block; width: auto;"
-    },
-    "message": {
-        "type": "h2",
-        "class": "",
-        "id": "",
-        "style": "text-align: center;"
-    },
-    "captchaImage": {
-        "type": "img",
-        "class": "",
-        "style": "padding: 5px; border: 0px solid #fff; margin-top: 5px; width: 32px; height: 32px; cursor: pointer; display:inline;",
-        "style:active": "padding: 5px; border: 3px solid #ccc; margin-top: 5px; width: 32px; height: 32px; cursor: pointer;"
-    }
-};
-!function(e,t,n,c,r,a,i){e.Newsletter2GoTrackingObject=r,e[r]=e[r]||function(){(e[r].q=e[r].q||[]).push(arguments)},e[r].l=1*new Date,a=t.createElement(n),i=t.getElementsByTagName(n)[0],a.async=1,a.src=c,i.parentNode.insertBefore(a,i)}(window,document,"script","https://static.newsletter2go.com/utils.js","n2g");
-n2g('create' ,'atuv6z83-fvdp22mb-jme');
-n2g('subscribe:createForm', config);
-</script>
+Vue.component("google-maps-widget",
+    {
+        template: `<div :class="aspectRatio" class="maps-component" ref="googleMapsContainer"></div>`,
+
+        props:
+            {
+                googleApiKey:
+                    {
+                        type: String,
+                        required: true
+                    },
+                address:
+                    {
+                        type: String,
+                        required: false
+                    },
+                lat:
+                    {
+                        type: Number
+                    },
+                lng:
+                    {
+                        type: Number
+                    },
+                zoom:
+                    {
+                        type: Number,
+                        default: 16
+                    },
+                aspectRatio:
+                    {
+                        type: String,
+                        default: "map-3-1",
+                        validator: value =>
+                        {
+                            return ["map-3-1", "map-2-1", "map-1-1", ""].indexOf(value) !== -1
+                        }
+                    }
+            },
+
+        computed:
+            {
+                coordinates()
+                {
+                    const isLatValid = !isNaN(this.lat) && this.lat > -90 && this.lat < 90
+                    const isLngValid = !isNaN(this.lng) && this.lng > -180 && this.lng < 180
+
+                    if (isLatValid && isLngValid)
+                    {
+                        return {
+                            lat: this.lat,
+                            lng: this.lng
+                        }
+                    }
+
+                    return null
+                }
+            },
+
+        mounted()
+        {
+            this.$nextTick(() =>
+            {
+                if (!document.querySelector("#google-maps-api"))
+                {
+                    this.createScript().then(() => this.initializeMap())
+                }
+                else
+                {
+                    this.listenToExistingScript()
+                }
+            })
+        },
+
+        methods:
+            {
+                createScript()
+                {
+                    return new Promise((resolve, reject) =>
+                    {
+                        const script = document.createElement("script")
+                        const scriptSource = `https://maps.googleapis.com/maps/api/js?key=${this.googleApiKey}`
+
+                        script.type = "text/javascript"
+                        script.id = "google-maps-api"
+                        script.src = scriptSource
+
+                        script.addEventListener("load", () => resolve(script), false)
+                        script.addEventListener("error", () => reject(script), false)
+
+                        document.body.appendChild(script)
+                    })
+                },
+
+                listenToExistingScript()
+                {
+                    const script = document.querySelector("script#google-maps-api")
+
+                    if (typeof google === "undefined")
+                    {
+                        script.addEventListener("load", () => this.initializeMap(), false)
+                    }
+                    else
+                    {
+                        this.initializeMap()
+                    }
+                },
+
+                initializeMap()
+                {
+                    if (this.coordinates)
+                    {
+                        this.renderMap(this.coordinates)
+                    }
+                    else
+                    {
+                        this.geocodeAddress().then(coordinates =>
+                        {
+                            this.renderMap(coordinates)
+                        });
+                    }
+                },
+
+                geocodeAddress()
+                {
+                    return new Promise((resolve, reject) =>
+                    {
+                        new google.maps.Geocoder().geocode({ address: this.address }, (results, status) =>
+                        {
+                            if (status === google.maps.GeocoderStatus.OK)
+                            {
+                                resolve({
+                                    lat: results[0].geometry.location.lat(),
+                                    lng: results[0].geometry.location.lng()
+                                })
+                            }
+                            else
+                            {
+                                reject()
+                            }
+                        })
+                    })
+                },
+
+                renderMap(coordinates)
+                {
+                    const map = new google.maps.Map(this.$refs.googleMapsContainer,
+                        {
+                            center: coordinates,
+                            zoom  : this.zoom
+                        })
+
+                    new google.maps.Marker(
+                        {
+                            map: map,
+                            position: coordinates
+                        })
+                }
+            }
+    })

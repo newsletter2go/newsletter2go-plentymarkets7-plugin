ddb27ce6d7ca27b26851859c005926ced13a3f36
MiMo <moritz@newsletter2go.com>
2019-01-23 15:35:03 +0100

    back to working state

--- src/Widgets/FormWidget.php
+++ src/Widgets/FormWidget.php
@@ -14,10 +14,6 @@ class FormWidget extends BaseWidget
         $formKey = $widgetSettings["formKey"]["mobile"];
         $formConfig = $widgetSettings["formConfig"]["mobile"];
 
-        if(!isset($formConfig)){
-            $formConfig = '{"container": {"type": "div","class": "","style": ""},"row": {"type": "div","class": "","style": "margin-top: 15px;"},"columnLeft": {"type": "div","class": "","style": ""},"columnRight": {"type": "div","class": "","style": ""},"button": {"type": "button","style": "background-color: #000000;"},"label": {"type": "label","class": "","style": ""}}';
-        }
-
         if (empty($formKey))
         {
             return [

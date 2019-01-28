b3a175d4499b6d408edecb4550735e02e02e4eb9
MiMo <moritz@newsletter2go.com>
2019-01-28 13:57:57 +0100

    missing class declaration

--- src/Providers/FormServiceProvider.php
+++ src/Providers/FormServiceProvider.php
@@ -7,6 +7,7 @@ use IO\Helper\ResourceContainer;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
+use Plenty\Plugin\RouteServiceProvider as BaseRouteServiceProvider;
  
 class FormServiceProvider extends BaseRouteServiceProvider
 {
